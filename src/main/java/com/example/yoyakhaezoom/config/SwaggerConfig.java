cd3250d8e8163f78fd876bdb5e1b0ecf97471ed8
sanghyeon lim <i48281973@gmail.com>
2025-06-25 19:59:48 +0900

    19:59

--- src/main/java/com/example/yoyakhaezoom/config/SwaggerConfig.java
+++ src/main/java/com/example/yoyakhaezoom/config/SwaggerConfig.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfig {
@@ -20,18 +20,15 @@ public class SwaggerConfig {
                 .version("v1.0.0")
                 .description("청소년을 위한 뉴스 요약 서비스 '요약해줌'의 API 명세서입니다.");
 
-        String jwtSchemeName = "bearerAuth";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
-        Components components = new Components()
-                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
-                        .name(jwtSchemeName)
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT"));
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
         return new OpenAPI()
-                .info(info)
-                .components(components)
-                .security(List.of(securityRequirement));
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
+                .info(info);
     }
 }
